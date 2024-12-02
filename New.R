0b206f9ab4b4fc1c79c3f0a0860218fc1d6a4ed8
Miksza <pmiksza@Mac.attlocal.net>
2024-12-02 17:20:40 -0500

    This is an attempt to upload a draft of the tutorial package.

--- New.R
+++ New.R
@@ -1,1 +1,5 @@
-This is a new file. And yet anbother change.
+# Code for setting up the package
+usethis::create_package("/Users/pmiksza/Library/CloudStorage/OneDrive-IndianaUniversity/2023-24 Teaching Folders/UMD Methods Day/AERMS")
+
+# Code for setting up the tutorial
+usethis::use_tutorial("AERMS_R_Tutorial", "Arts Education Research Methods Summit: Intro to R", open = interactive())
